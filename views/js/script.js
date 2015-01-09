02977a684d97c94e6d913b438c09540d12e20c8a
johannesbraun <braun.johannes@gmail.com>
2015-01-09 11:24:57 +0000

    main in script.js

--- views/js/script.js
+++ views/js/script.js
@@ -1,17 +1,24 @@
+var main = function () {
+
 SC.initialize({
-  client_id: '340f063c670272fac27cfa67bffcafc4',
-  redirect_uri: 'http://external.codecademy.com/soundcloud.html'
+  client_id: '17089d3c7d05cb2cfdffe46c2e486ff0',
+  redirect_uri: 'http://jb-blasta-me-staging.herokuapp.com/callback.html'
 });
 
 $(document).ready(function() {
   $('#username').html("fhgf");
   $('a.connect').click(function(e) {
-    //e.preventDefault();
+    e.preventDefault();
     SC.connect(function() {
         $('#username').append("bbbb");
         SC.get('/me',function(me){
                 $('#username').html("Du bisch der "+me.username);
+                alert('Hello' + me.username);
             });
         });
     });
  });
+
+}
+
+$(document).ready(main);
