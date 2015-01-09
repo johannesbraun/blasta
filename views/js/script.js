82f4e38a1816c52472e04429c3aae7a236a5f1cd
johannesbraun <braun.johannes@gmail.com>
2015-01-09 11:39:57 +0000

    create playlists

--- views/js/script.js
+++ views/js/script.js
@@ -1,11 +1,12 @@
+
 var main = function () {
 
-SC.initialize({
+  SC.initialize({
   client_id: '17089d3c7d05cb2cfdffe46c2e486ff0',
   redirect_uri: 'http://jb-blasta-me-staging.herokuapp.com/callback.html'
-});
+  });
 
-$(document).ready(function() {
+  $(document).ready(function() {
   $('#username').html("fhgf");
   $('a.connect').click(function(e) {
     e.preventDefault();
@@ -16,9 +17,17 @@ $(document).ready(function() {
                 alert('Hello' + me.username);
             });
         });
-    });
- });
+    })  ;
+  $('a.createPL').click(function(e) {
+    SC.connect(function() {
+    var tracks = [22448500, 21928809].map(function(id) { return { id: id } });
+    SC.post('/playlists', {
+      playlist: { title: 'My Playlist', tracks: tracks }
+    }); 
+  });
+  });
 
-}
+  });
+};
 
 $(document).ready(main);
